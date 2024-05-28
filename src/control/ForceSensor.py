123946159c2babcbb470ea2ada25e791ce6c04d7
Taegyeom <grwise0906@gamil.com>
2024-05-28 14:32:03 +0900

    test

--- src/control/ForceSensor.py
+++ src/control/ForceSensor.py
@@ -10,8 +10,8 @@ class ForceSensor:
         self.RIGHT_FRONT_PIN = force_params.RIGHT_FRONT_PIN
         self.RIGHT_BACK_PIN = force_params.RIGHT_BACK_PIN
 
-        self.ADC = ADS1x15.ADS1115(self.I2C_BUS_ID, 0x48)
-        self.ADC.setGain(self.ADC.PGA_4_096V)
+        self.ADS = ADS1x15.ADS1115(self.I2C_BUS_ID, 0x48)
+        self.ADS.setGain(self.ADS.PGA_4_096V)
         
         self.right = None
         self.left = None
@@ -20,10 +20,10 @@ class ForceSensor:
         self.COMPARE_VALUE = 5000
 
     def measure_value(self):
-        left_front_value = self.ADC.readADC(self.LEFT_FRONT_PIN)
-        left_back_value = self.ADC.readADC(self.LEFT_BACK_PIN)
-        right_front_value = self.ADC.readADC(self.RIGHT_FRONT_PIN)
-        rigth_back_value = self.ADC.readADC(self.RIGHT_BACK_PIN)
+        left_front_value = self.ADS.readADC(self.LEFT_FRONT_PIN)
+        left_back_value = self.ADS.readADC(self.LEFT_BACK_PIN)
+        right_front_value = self.ADS.readADC(self.RIGHT_FRONT_PIN)
+        rigth_back_value = self.ADS.readADC(self.RIGHT_BACK_PIN)
 
         return left_front_value, left_back_value, right_front_value, rigth_back_value
 
