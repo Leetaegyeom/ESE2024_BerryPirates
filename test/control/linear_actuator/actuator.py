f4cb0b0ae63293fbaf3d4621ef10d52909f2baa5
Taegyeom <grwise0906@gamil.com>
2024-06-03 14:49:08 +0900

    edit test

--- test/control/linear_actuator/actuator.py
+++ test/control/linear_actuator/actuator.py
@@ -12,39 +12,40 @@ ENA_PIN_1 = 25  # GPIO pin connected to the EN1 pin L298N
 IN1_PIN_1 = 8  # GPIO pin connected to the IN1 pin L298N
 IN2_PIN_1 = 7  # GPIO pin connected to the IN2 pin L298N
 
-ENA_PIN_2 = 17 # GPIO pin connected to the EN1 pin L298N
-IN1_PIN_2 = 27  # GPIO pin connected to the IN1 pin L298N
-IN2_PIN_2 = 22  # GPIO pin connected to the IN2 pin L298N
+# ENA_PIN_2 = 17 # GPIO pin connected to the EN1 pin L298N
+# IN1_PIN_2 = 27  # GPIO pin connected to the IN1 pin L298N
+# IN2_PIN_2 = 22  # GPIO pin connected to the IN2 pin L298N
 
 # Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ENA_PIN_1, GPIO.OUT)
 GPIO.setup(IN1_PIN_1, GPIO.OUT)
 GPIO.setup(IN2_PIN_1, GPIO.OUT)
-GPIO.setup(ENA_PIN_2, GPIO.OUT)
-GPIO.setup(IN1_PIN_2, GPIO.OUT)
-GPIO.setup(IN2_PIN_2, GPIO.OUT)
+# GPIO.setup(ENA_PIN_2, GPIO.OUT)
+# GPIO.setup(IN1_PIN_2, GPIO.OUT)
+# GPIO.setup(IN2_PIN_2, GPIO.OUT)
 
 # Set ENA_PIN_1 to HIGH to enable the actuator
 GPIO.output(ENA_PIN_1, GPIO.HIGH)
-GPIO.output(ENA_PIN_2, GPIO.HIGH)
+# GPIO.output(ENA_PIN_2, GPIO.HIGH)
 
 # Main loop
 try:
     while True:
         # Extend the actuator
-        GPIO.output(IN1_PIN_1, GPIO.HIGH)
-        GPIO.output(IN2_PIN_1, GPIO.LOW)
-        GPIO.output(IN1_PIN_2, GPIO.HIGH)
-        GPIO.output(IN2_PIN_2, GPIO.LOW)
+        print("1")
+        # GPIO.output(IN1_PIN_1, GPIO.HIGH)
+        # GPIO.output(IN2_PIN_1, GPIO.LOW)
+        # # GPIO.output(IN1_PIN_2, GPIO.HIGH)
+        # # GPIO.output(IN2_PIN_2, GPIO.LOW)
 
-        time.sleep(20)  # Actuator will stop extending automatically when reaching the limit
+        # time.sleep(20)  # Actuator will stop extending automatically when reaching the limit
 
         # Retract the actuator
         GPIO.output(IN1_PIN_1, GPIO.LOW)
         GPIO.output(IN2_PIN_1, GPIO.HIGH)
-        GPIO.output(IN1_PIN_2, GPIO.LOW)
-        GPIO.output(IN2_PIN_2, GPIO.HIGH)
+        # GPIO.output(IN1_PIN_2, GPIO.LOW)
+        # GPIO.output(IN2_PIN_2, GPIO.HIGH)
 
         time.sleep(20)  # Actuator will stop retracting automatically when reaching the limit
 
